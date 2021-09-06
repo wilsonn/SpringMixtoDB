5b74d613922a4ae9f6c14d2f32f67a6104a9e4fe
Wilson Hernande Neira Mija <wilsonnm22@hotmail.com>
2021-09-06 00:13:18 -0500

    modificando gitignore

---
+++ src/main/java/com/whnm/model/Persona.java
@@ -0,0 +1,28 @@
+package com.whnm.model;
+
+public class Persona {
+	private int id;
+	private String nombres;
+	private String apellidos;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNombres() {
+		return nombres;
+	}
+	public void setNombres(String nombres) {
+		this.nombres = nombres;
+	}
+	public String getApellidos() {
+		return apellidos;
+	}
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+	
+	
+}
